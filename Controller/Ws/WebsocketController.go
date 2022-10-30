648fc6d54c98712637f7c3776d224a3b0f57e8b2
mjy191 <mjy191@163.com>
2022-10-30 22:14:10 +0800

    增加指定用发送消息

--- Controller/Ws/WebsocketController.go
+++ Controller/Ws/WebsocketController.go
@@ -1,6 +1,7 @@
 package Ws
 
 import (
+	"encoding/json"
 	"fmt"
 	"ginApi/Controller"
 	"github.com/gin-gonic/gin"
@@ -22,6 +23,13 @@ var upgrader = websocket.Upgrader{
 }
 
 var conns []*websocket.Conn
+var connMap = make(map[string]*websocket.Conn)
+
+type userInfo struct {
+	Uuid string `json:"uuid""`
+}
+
+var user userInfo
 
 var msg = make(chan string, 20)
 
@@ -31,6 +39,8 @@ func (this WebsocketController) Handel(c *gin.Context) {
 		log.Println(err)
 		return
 	}
+	//报错uuid和conn
+	//connMap[uuid] = conn
 	// 保存所有的链接
 	msg <- "有人上线：" + fmt.Sprintf("%v", conn.RemoteAddr())
 	go broadcast(conns)
@@ -41,7 +51,13 @@ func (this WebsocketController) Handel(c *gin.Context) {
 		if e != nil {
 			break
 		}
+		if er := json.Unmarshal(p, &user); er == nil {
+			connMap[user.Uuid] = conn
+		}
+		//保存对应关系
+		log.Println("connMap", connMap)
 		// 遍历链接，广播发送消息
+		log.Println(string(p))
 		msg <- fmt.Sprintf("%v：%s", conn.RemoteAddr(), string(p))
 		go broadcast(conns)
 	}
@@ -55,6 +71,13 @@ func (this WebsocketController) Handel(c *gin.Context) {
 		}
 	}
 	conns = append(conns[:index], conns[index+1:]...)
+	// 删除connmap
+	for key, val := range connMap {
+		if val == conn {
+			delete(connMap, key)
+			break
+		}
+	}
 	defer conn.Close()
 	log.Println(conns)
 	log.Println("服务关闭")
@@ -68,6 +91,11 @@ func broadcast(conns []*websocket.Conn) {
 	}
 }
 
+//单发消息
+func singleMsg(conn *websocket.Conn, msg string) {
+	conn.WriteMessage(websocket.TextMessage, []byte(msg))
+}
+
 // 通过http协议推送消息
 func (this WebsocketController) SendMsg(c *gin.Context) {
 	// 从body获取消息推送
@@ -75,11 +103,22 @@ func (this WebsocketController) SendMsg(c *gin.Context) {
 	//b, _ := io.ReadAll(body)
 	//body.Close()
 	//fmt.Println(string(b))
+
 	// 从form表单获取消息推送
-	b := c.Request.FormValue("msg")
-	fmt.Println(b)
-	msg <- b
-	fmt.Println(conns)
-	go broadcast(conns)
+	// 群发消息
+	//b := c.Request.FormValue("msg")
+	//fmt.Println(b)
+	//msg <- b
+	//fmt.Println(conns)
+	//go broadcast(conns)
+
+	//发送单个消息
+	mg := c.Request.FormValue("msg")
+	uuid := c.Request.FormValue("uuid")
+	log.Println(connMap)
+	conn, ok := connMap[uuid]
+	if ok {
+		singleMsg(conn, mg)
+	}
 	c.Writer.Write([]byte(fmt.Sprintf("%v", "发送成功")))
 }
